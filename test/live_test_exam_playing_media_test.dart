272b5ce40944d0b814a00c4f801ac0a80f5bc90b
codewithjoypahan <joypahan7666@gmail.com>
2024-06-24 23:40:45 +0600

    live test exam commit

---
+++ test/live_test_exam_playing_media_test.dart
@@ -0,0 +1,8 @@
+import 'package:live_test_exam_playing_media/live_test_exam_playing_media.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
